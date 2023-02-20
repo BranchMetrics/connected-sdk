3d0cfa7526413048307f86f3c5693909df482a78
Jagadeesh Karicherla <jagadeesh.karicherla@branch.io>
2023-02-20 15:39:56 -0800

    fix example page src

--- test/web-config.js
+++ test/web-config.js
@@ -6,6 +6,6 @@
 config.app_service_endpoint = 'https://app.link';
 config.link_service_endpoint = 'https://bnc.lt';
 config.api_endpoint = 'https://api2.branch.io';
-config.version = '1.2.0';
+config.version = '1.2.1';
 config.sdk='connected';
 
