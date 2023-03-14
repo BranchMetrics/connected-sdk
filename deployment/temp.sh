573c2d9a7758dcba67937881aa203ff39928b1e8
Jagadeesh Karicherla <jagadeesh.karicherla@branch.io>
2023-03-14 09:45:54 -0700

    change acl permissions

--- deployment/temp.sh
+++ deployment/temp.sh
@@ -1,7 +1,7 @@
   echo -en "${GREEN}QA Release...${NC}\n"
 
   echo -en "${GREEN}Pushing to S3: branch-builds ...${NC}\n"
-  aws s3 cp --content-type="text/javascript" --content-encoding="gzip" temp/build.min.js.gz s3://branch-builds/websdk/branch-2.71.0.min.js --acl public-read
+  aws s3 cp --content-type="text/javascript" --content-encoding="gzip" temp/build.min.js.gz s3://branch-builds/websdk/branch-2.71.0.min.js
   aws s3 cp --content-type="text/javascript" --content-encoding="gzip" temp/build.min.js.gz s3://branch-builds/websdk/branch-latest.min.js --acl public-read
   aws s3 cp --content-type="text/javascript" --content-encoding="gzip" temp/build.min.js.gz s3://branch-builds/websdk/branch-v2.0.0.min.js --acl public-read
   aws s3 cp --content-type="text/javascript" temp/build.js s3://branch-builds/websdk/branch.js --acl public-read
