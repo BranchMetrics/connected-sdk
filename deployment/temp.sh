280a4256167592f9c88504e0cf0246e6849d1228
Jagadeesh Karicherla <jagadeesh.karicherla@branch.io>
2023-03-28 21:17:30 -0700

    release 2.73.0

--- deployment/temp.sh
+++ deployment/temp.sh
@@ -1,9 +1,14 @@
   echo -en "${GREEN}QA Release...${NC}\n"
 
   echo -en "${GREEN}Pushing to S3: branch-builds ...${NC}\n"
-  aws s3 cp --content-type="text/javascript" --content-encoding="gzip" temp/build.min.js.gz s3://branch-builds/websdk/branch-2.72.0.min.js
+  aws s3 cp --content-type="text/javascript" --content-encoding="gzip" temp/build.min.js.gz s3://branch-builds/websdk/branch-2.73.0.min.js
   aws s3 cp --content-type="text/javascript" --content-encoding="gzip" temp/build.min.js.gz s3://branch-builds/websdk/branch-latest.min.js
   aws s3 cp --content-type="text/javascript" --content-encoding="gzip" temp/build.min.js.gz s3://branch-builds/websdk/branch-v2.0.0.min.js
   aws s3 cp --content-type="text/javascript" temp/build.js s3://branch-builds/websdk/branch.js
   aws s3 cp temp/example.html s3://branch-builds/websdk/example.html
+
+  echo -en "${GREEN}Pushing to S3: branch-cdn ...${NC}\n"
+  aws s3 cp --content-type="text/javascript" --content-encoding="gzip" temp/build.min.js.gz s3://branch-cdn/branch-2.73.0.min.js --cache-control "max-age=300"
+  aws s3 cp --content-type="text/javascript" --content-encoding="gzip" temp/build.min.js.gz s3://branch-cdn/branch-latest.min.js --cache-control "max-age=300"
+  aws s3 cp temp/example.html s3://branch-cdn/example.html
 
