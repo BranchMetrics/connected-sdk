58fcb61db8ca693d81415c9fd9afba7ea5d34500
Jagadeesh Karicherla <jagadeesh.karicherla@branch.io>
2024-01-11 15:30:55 -0800

    refactor(core): remove journey assist code
    remove journey assist code

--- src/6_branch.js
+++ src/6_branch.js
@@ -225,30 +225,12 @@ Branch.prototype._api = function(resource, obj, callback) {
 /***
  * @function Branch._referringLink
  */
-Branch.prototype._referringLink = function(forJourneys) {
+Branch.prototype._referringLink = function() {
 	var sessionData = session.get(this._storage);
 	var referringLink = sessionData && sessionData['referring_link'];
 	if (referringLink) {
 		return referringLink;
 	}
-	else {
-		if (utils.userPreferences.enableExtendedJourneysAssist && forJourneys) {
-			var localStorageData = session.get(this._storage, true);
-			var referring_Link = localStorageData && localStorageData['referring_link'];
-			var referringLinkExpiry = localStorageData && localStorageData['referringLinkExpiry'];
-			if (referring_Link && referringLinkExpiry) {
-				var now = new Date();
-				// compare the expiry time of the item with the current time
-				if (now.getTime() > referringLinkExpiry) {
-					session.patch(this._storage, { "referringLinkExpiry": null }, true, true);
-				}
-				else {
-					return referring_Link;
-				}
-			}
-		}
-	}
-
 	var clickId = this._storage.get('click_id');
 	if (clickId) {
 		return config.link_service_endpoint + '/c/' + clickId;
@@ -352,8 +334,6 @@ Branch.prototype['init'] = wrap(
 		utils.debug = options && options['enableLogging'] ? options['enableLogging'] : utils.debug;
 
 		utils.userPreferences.trackingDisabled = options && options['tracking_disabled'] && options['tracking_disabled'] === true ? true : false;
-		utils.userPreferences.enableExtendedJourneysAssist = options && options['enableExtendedJourneysAssist'] ? options['enableExtendedJourneysAssist'] : utils.userPreferences.enableExtendedJourneysAssist;
-		utils.extendedJourneysAssistExpiryTime = options && options['extendedJourneysAssistExpiryTime'] && Number.isInteger(options['extendedJourneysAssistExpiryTime']) ? options['extendedJourneysAssistExpiryTime'] : utils.extendedJourneysAssistExpiryTime;
 		utils.userPreferences.allowErrorsInCallback = false;
 		utils.getClientHints();
 
