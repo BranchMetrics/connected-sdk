c12888a296853eb0fc3ba07b2fd7b57bc0201c9e
chrisyeagy-branch <christopher.yeagy@branch.io>
2025-08-07 16:32:01 -0700

    Update master_builds.sh

--- deployment/master_builds.sh
+++ deployment/master_builds.sh
@@ -25,11 +25,11 @@ if [ "$CIRCLE_BRANCH" == 'master' ]; then
   make release
 
   echo -en "${GREEN}Pushing to S3: branch-builds ...${NC}\n"
-  aws s3 cp --content-type="text/javascript" --content-encoding="gzip" dist/build.min.js.gz s3://branch-builds/websdk/branch-$VERSION.min.js --acl public-read
-  aws s3 cp --content-type="text/javascript" --content-encoding="gzip" dist/build.min.js.gz s3://branch-builds/websdk/branch-latest.min.js --acl public-read
-  aws s3 cp --content-type="text/javascript" --content-encoding="gzip" dist/build.min.js.gz s3://branch-builds/websdk/branch-v2.0.0.min.js --acl public-read
-  aws s3 cp --content-type="text/javascript" dist/build.js s3://branch-builds/websdk/branch.js --acl public-read
-  aws s3 cp example.html s3://branch-builds/websdk/example.html --acl public-read
+  aws s3 cp --content-type="text/javascript" --content-encoding="gzip" dist/build.min.js.gz s3://branch-builds-usw2/websdk/branch-$VERSION.min.js --acl public-read
+  aws s3 cp --content-type="text/javascript" --content-encoding="gzip" dist/build.min.js.gz s3://branch-builds-usw2/websdk/branch-latest.min.js --acl public-read
+  aws s3 cp --content-type="text/javascript" --content-encoding="gzip" dist/build.min.js.gz s3://branch-builds-usw2/websdk/branch-v2.0.0.min.js --acl public-read
+  aws s3 cp --content-type="text/javascript" dist/build.js s3://branch-builds-usw2/websdk/branch.js --acl public-read
+  aws s3 cp example.html s3://branch-builds-usw2/websdk/example.html --acl public-read
 
 else
 
